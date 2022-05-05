929d73a8993c07da4aa31adab0c7f15851713dc2
Diego ZF <ddzadravec@gmail.com>
2022-05-05 04:37:19 -0300

    feat: iniciando pagina de registro

---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../../components/Header";
+
+import { Container } from "./style";
+
+export const Register = () => {
+  return (
+    <Container>
+      <Header hiddenInfoUser height={14} title="Registro"/>
+    </Container>
+  );
+};
