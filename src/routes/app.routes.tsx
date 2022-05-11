056325e0d7836621fb0dd8c387a2dd29083061b4
Diego ZF <ddzadravec@gmail.com>
2022-05-11 20:15:50 -0300

    feat: finalizando implemntação de rotas

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-
+import { Platform } from "react-native";
+import { useTheme } from "styled-components";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -8,11 +10,59 @@ import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
 
 export const AppRoutes = () => {
+  const theme = useTheme();
   return (
-    <Navigator>
-      <Screen name="Dashboard" component={Dashboard} />
-      <Screen name="Register" component={Register} />
-      <Screen name="Resume" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: "beside-icon",
+        tabBarStyle: {
+          height: 60,
+          paddingVertical: Platform.OS === "ios" ? 20 : 0,
+        },
+      }}
+    >
+      <Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="format-list-bulleted"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Register"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="attach-money"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Screen
+        name="Resume"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialIcons
+              name="pie-chart"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Navigator>
   );
 };
