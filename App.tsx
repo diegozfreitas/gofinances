1fd01e723a585db90b37ca8ecb46a67abc1d6efc
Diego ZF <ddzadravec@gmail.com>
2022-05-17 17:16:23 -0300

    chore: configurando o intl

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components";
+import 'intl'
+import 'intl/locale-data/jsonp/pt-BR'
 
 import {
   useFonts,
