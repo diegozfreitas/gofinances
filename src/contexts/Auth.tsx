507aa9427c0e1f6c18ca43a703704dae34119c73
Diego ZF <ddzadravec@gmail.com>
2022-05-23 12:57:38 -0300

    feat: criando recurso de autenticação com google

--- src/contexts/Auth.tsx
+++ src/contexts/Auth.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, ReactNode } from "react";
+import * as AuthSession from "expo-auth-session";
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -12,6 +13,7 @@ interface User {
 
 interface AuthContextData {
   user: User;
+  singInWithGoogle: () => Promise<void>;
 }
 
 const AuthContext = createContext({} as AuthContextData);
@@ -24,8 +26,25 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
     photo: "",
   };
 
+  const singInWithGoogle = async () => {
+    try {
+      const CLIENT_ID =
+        "282684136250-vdcdruhl2rdqnjf2pg087g9g8tj2inp2.apps.googleusercontent.com";
+      const REDIRECT_URI = "https://auth.expo.io/@ddzadravec/gofinances";
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("profile email");
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const response = await AuthSession.startAsync({ authUrl });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user: user }}>
+    <AuthContext.Provider value={{ user, singInWithGoogle }}>
       {children}
     </AuthContext.Provider>
   );
